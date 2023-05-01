cde923916985c386acf4dff4d1f987cf7aa5538f
sromeros3 <117762738+sromeros3@users.noreply.github.com>
2023-05-01 10:34:38 +0200

    Configuration class
    Configuration needed to run Springboot

--- calculator/calculator/src/main/java/com/micro/calculator/CalculatorApplication.java
+++ calculator/calculator/src/main/java/com/micro/calculator/CalculatorApplication.java
@@ -1,11 +1,22 @@
 package com.micro.calculator;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
-public class CalculatorApplication {
+@Configuration
+@ComponentScan(basePackages = "com.micro.calculator")
+@EnableAutoConfiguration
+public class CalculatorApplication extends SpringBootServletInitializer{
 
+	@Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	        return builder.sources(ConfigAplicacion.class);
+        }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
